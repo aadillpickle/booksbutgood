c5210af9ab620a4067d1cf510ace0bd709e0a67a
Aadil Ali <aliaadil2002@gmail.com>
2023-01-01 01:38:46 -0500

    its fucking lit

--- frontend/src/pages/Chapter.js
+++ frontend/src/pages/Chapter.js
@@ -1,14 +1,32 @@
 import 'react-chat-elements/dist/main.css'
-import { MessageBox, MessageList, Input, Button } from 'react-chat-elements'
+import { MessageList, Input, Button } from 'react-chat-elements'
 import React, { useRef, useState, useEffect } from 'react';
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate, redirect, Link } from "react-router-dom";
 
 
+const searchInBook = async (indexedContent) => {
+  const chapterIds = [54, 55, 60, 61, 62, 63, 70, 65, 64, 66, 58, 67, 68, 71, 69, 57]
+  const options = {method: 'GET'};
+  const chapters = [];
+  await Promise.all (chapterIds.map(async (id) => {
+    const chapterContent = await fetch(process.env.REACT_APP_API_ROOT + '/chapter/' + id, options)
+    const response = await chapterContent.json()
+    const chapter = response.content
+    if (chapter.includes(indexedContent[0].substring(0, 20)) || chapter.includes(indexedContent[1].substring(0, 20))) {
+      chapters.push({"id": id, "title": response.title})
+    }
+  }))
+  return chapters;
+}
 
 const searchWithin = async (input) => {
-  const options = {method: 'GET'};
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: `{"question":"${input}"}`
+  };
 
-  const result = await fetch('http://localhost:3001/operand', options)
+  const result = await fetch(process.env.REACT_APP_API_ROOT + '/operand/search', options)
   const response = await result.json()
   console.log(response)
   return response;
@@ -20,7 +38,22 @@ function Chapter() {
   const messageListReferance = useRef(null)
   const [messages, setMessages] = useState([])
 
-  const handleClick = (input) => {
+  const routeChange = (pathToNavigate) => {
+    redirect("/");
+    redirect(pathToNavigate);
+  }
+  const handleClick = async (input) => {
+    const response = await searchWithin(input);
+    const chapters = await searchInBook(response[0]);
+
+    let botText = '';
+    if (response[1] !== '') {
+      botText = <p>{response[1]} You can read more in <button style={{color: 'blue'}} onClick={() => {setChapterId(chapters[0].id); routeChange(`chapter/${chapters[0].id}`)}}>Chapter {chapters[0].title}</button> or <button style={{color: 'blue'}} onClick={() => {setChapterId(chapters[1].id); routeChange(`chapter/${chapters[1].id}`)}}>Chapter {chapters[1].title}</button></p>
+    }
+    else {
+      botText = <p>You can read more in <button style={{color: 'blue'}} onClick={() => {setChapterId(chapters[0].id); routeChange(`chapter/${chapters[0].id}`)}}>Chapter {chapters[0].title}</button> or <button style={{color: 'blue'}} onClick={() => {setChapterId(chapters[1].id); routeChange(`chapter/${chapters[1].id}`)}}>Chapter {chapters[1].title}</button></p>
+    }
+
     const userChatObj = {
       position: 'right',
       type: 'text',
@@ -30,10 +63,9 @@ function Chapter() {
     const botChatObj = {
       position: 'left',
       type: 'text',
-      text: searchWithin(input),
+      text: botText,
       date: new Date(),
     }
-    console.log('wazup')
     setMessages([...messages, userChatObj, botChatObj])
     inputRef.current.value = ''
   }
@@ -50,7 +82,6 @@ function Chapter() {
       .then((data) => {
         setBook(data);
         if (!chapterId) setChapterId(data.chapters[0].id)
-
       })
   }, [bookId])
 
@@ -95,7 +126,7 @@ function Chapter() {
 
         <div className="flex flex-col mt-5">
           {book?.chapters ? book.chapters.map((chapter)=>{
-            
+
             return <Link key={chapter.order} to={`/chapter/${chapter.id}`}>
             <div  className="hover:bg-stone-200 text-sm rounded py-1 cursor-pointer px-3 transition-colors" onClick={()=>setChapterId(chapter.id)}>{chapter.title}</div>
           </Link>
@@ -124,12 +155,13 @@ function Chapter() {
           <article id="content" dangerouslySetInnerHTML={{ __html: chapter?.content }} className="px-12 py-16 max-w-2xl mx-auto prose prose lg:prose-xl"/>
         </div>
         <div id="right" className="flex flex-col w-96 ml-auto bg-blue-100 h-full">
-          <div className="h-1/6" id="progress">
+          {/* <div className="h-1/6" id="progress">
           quiz progress
           </div>
           <div className="h-2/6" id="question">
           quiz question
-          </div>
+          </div> */}
+          <div className="flex flex-row items-center justify-center h-3/6 m-4 font-sans bg-white rounded" id="coming-soon"><p>Quizzes and flashcards coming soon</p></div>
           <div className="flex flex-col h-3/6 justify-between" id="chat">
           <MessageList
             referance={messageListReferance}
