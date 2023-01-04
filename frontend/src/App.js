3895afb03600f660fe758f8b3f9f9423fd658d75
Aadil Ali <aliaadil2002@gmail.com>
2023-01-04 15:31:31 -0500

    added ga

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import 'react-chat-elements/dist/main.css'
 import Chapter from "./pages/Chapter.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-BFGZ10KN5');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   return (
