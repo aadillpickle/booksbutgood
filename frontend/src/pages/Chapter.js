02aac85d544bb83c3c00c6d2b0b1f6dc189f8b80
ri <riley.walz@macaulay.cuny.edu>
2023-01-04 01:32:54 +0000

    push frontend

--- frontend/src/pages/Chapter.js
+++ frontend/src/pages/Chapter.js
@@ -154,6 +154,23 @@ function Chapter() {
       });
   }, [chapterId]);
 
+  const [searchLoading, setSearchLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  async function search() {
+    setSearchLoading(true);
+    const chapterContent = await fetch(
+      process.env.REACT_APP_API_ROOT +
+        "/search?query=" +
+        encodeURIComponent(searchQuery),
+      { options: { method: "GET" } }
+    );
+    const response = await chapterContent.json();
+    setSearchResults(response);
+
+    setSearchLoading(false);
+  }
+
   const [mouseOverTeaser, setMouseOverTeaser] = useState(false);
   const [mouseOverTOC, setMouseOverTOC] = useState(false);
   const displayingTOC = useRef(false);
@@ -223,75 +240,105 @@ function Chapter() {
                 spellCheck="false"
                 placeholder="Search for anything"
                 required
+                onKeyDown={(e) => {
+                  setSearchQuery(e.target.value);
+                  if (!e.target.value) setSearchResults(null);
+                  if (e.keyCode === 13) {
+                    e.preventDefault();
+                    search();
+                  }
+                }}
               />
             </div>
           </form>
         </div>
 
-        <div className="flex flex-col mt-2">
-          {book?.chapters ? (
-            book.chapters.map((chapter) => {
-              return (
-                <Link key={chapter.order} to={`/chapter/${chapter.id}`}>
-                  <div
-                    className={
-                      "flex items-center text-sm rounded py-1 cursor-pointer px-3 transition-colors" +
-                      (chapter.id == chapterId
-                        ? " bg-stone-800 text-stone-200"
-                        : " hover:bg-stone-200")
-                    }
-                    onClick={() => setChapterId(chapter.id)}
-                  >
-                    <div>
-                      {chapter.title.includes(". ") ? (
-                        <>
-                          <span className="font-extrabold mr-2">
-                            {chapter.title.split(". ")[0]}
-                          </span>
-                          <span>{chapter.title.split(". ")[1]}</span>
-                        </>
-                      ) : (
-                        <>{chapter.title}</>
+        {searchResults && searchResults.length && (
+          <>
+            <p
+              className="text-sm ml-4 cursor-pointer"
+              onClick={() => setSearchResults(null)}
+            >
+              Clear search
+            </p>
+
+            <div className="flex flex-col mt-2">
+              {searchResults.map((result) => {
+                return (
+                  <div className="flex items-center text-sm rounded py-1 cursor-pointer px-3">
+                    {result.content}
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+        {!searchResults?.length && (
+          <div className="flex flex-col mt-2">
+            {book?.chapters ? (
+              book.chapters.map((chapter) => {
+                return (
+                  <Link key={chapter.order} to={`/chapter/${chapter.id}`}>
+                    <div
+                      className={
+                        "flex items-center text-sm rounded py-1 cursor-pointer px-3 transition-colors" +
+                        (chapter.id == chapterId
+                          ? " bg-stone-800 text-stone-200"
+                          : " hover:bg-stone-200")
+                      }
+                      onClick={() => setChapterId(chapter.id)}
+                    >
+                      <div>
+                        {chapter.title.includes(". ") ? (
+                          <>
+                            <span className="font-extrabold mr-2">
+                              {chapter.title.split(". ")[0]}
+                            </span>
+                            <span>{chapter.title.split(". ")[1]}</span>
+                          </>
+                        ) : (
+                          <>{chapter.title}</>
+                        )}
+                      </div>
+                      {loadingChapter == chapter.id && (
+                        <div id="loading-spinner" className="ml-auto">
+                          <div role="status">
+                            <svg
+                              aria-hidden="true"
+                              className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-stone-500"
+                              viewBox="0 0 100 101"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              />
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                              />
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                          </div>
+                        </div>
                       )}
                     </div>
-                    {loadingChapter == chapter.id && (
-                      <div id="loading-spinner" className="ml-auto">
-                        <div role="status">
-                          <svg
-                            aria-hidden="true"
-                            className="w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-stone-500"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="currentFill"
-                            />
-                          </svg>
-                          <span className="sr-only">Loading...</span>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </Link>
-              );
-            })
-          ) : (
-            <div className="animate-pulse">
-              <div className="hover:bg-stone-200 rounded py-2 px-3 transition-colors">
-                <div className="h-2 bg-stone-300  rounded col-span-2"></div>
+                  </Link>
+                );
+              })
+            ) : (
+              <div className="animate-pulse">
+                <div className="hover:bg-stone-200 rounded py-2 px-3 transition-colors">
+                  <div className="h-2 bg-stone-300  rounded col-span-2"></div>
+                </div>
+                <div className="hover:bg-stone-200 rounded py-2 px-3 transition-colors">
+                  <div className="h-2 bg-stone-300  rounded w-4/6"></div>
+                </div>
               </div>
-              <div className="hover:bg-stone-200 rounded py-2 px-3 transition-colors">
-                <div className="h-2 bg-stone-300  rounded w-4/6"></div>
-              </div>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </div>
       <div id="columns" className="flex overflow-hidden h-screen">
         <div
