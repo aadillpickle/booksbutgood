274a6bb1f4dfb1b2c43ba96f13110aac48a00613
Aadil Ali <aliaadil2002@gmail.com>
2023-01-25 13:40:45 -0500

    fixn anal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 
 const App = () => {
-	useEffect(() => {
-		if (process.env.NODE_ENV !== "production") return;
-		const script = document.createElement("script");
+	// useEffect(() => {
+	// 	const script = document.createElement("script");
 
-		script.src = "https://umani.api.route.run/umami.js";
-		script.async = true;
-		script["data-website-id"] = "68db3bde-1280-4154-9891-c6c2c8d0d080";
-		script.defer = true;
+	// 	script.src = "https://umani.api.route.run/umami.js";
+	// 	script.async = true;
+	// 	script["data-website-id"] = "68db3bde-1280-4154-9891-c6c2c8d0d080";
+	// 	script.defer = true;
 
-		document.body.appendChild(script);
+	// 	document.body.appendChild(script);
 
-		return () => {
-			document.body.removeChild(script);
-		};
-	}, []);
+	// 	// return () => {
+	// 	// 	document.body.removeChild(script);
+	// 	// };
+	// }, []);
 	return (
 		<Router>
 			<Routes>
